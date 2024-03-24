5e2237b4f0decc40d734ea4b4baea37efe8534cd
Roopesh Krishna <roopeshkrishna@Roopeshs-MacBook-Pro.local>
2024-03-24 22:34:33 +0530

    Instance creation

---
+++ Day 1/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "test" {
+  ami = "ami-080e1f13689e07408"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-"
+  key_name = "master"
+}
